1926f352e32784823a42d2869856a11d159bba64
Jayanth Chennamangalam <jayanthc@users.noreply.github.com>
2019-02-10 09:44:14 -0800

    Basic JS FFT.

--- jayanth_tests/load_wave.js
+++ jayanth_tests/load_wave.js
@@ -5,8 +5,8 @@ window.onload = function() {
         var reader = new FileReader();
         reader.onload = function() {
             var arrayBuffer = reader.result;
-            console.log("arrayBuffer:");
-            console.log(arrayBuffer);
+            //console.log("arrayBuffer:");
+            //console.log(arrayBuffer);
             audioContext.decodeAudioData(arrayBuffer, decodedDone);
         };
         reader.addEventListener('load', function() {
@@ -18,14 +18,42 @@ window.onload = function() {
     function decodedDone(decoded) {
         var typedArray = new Float32Array(decoded.length);
         typedArray = decoded.getChannelData(0);
-        console.log("typedArray:");
-        console.log(typedArray);
-        var a = nj.array(typedArray);
-        console.log(a);
-        //fft = nj.fft(a);
-        for (i = 0; i < 1000; i++) { 
-            value =typedArray[i]);
+        RI = nj.concatenate(nj.ones([10,1]), nj.zeros([10,1]));
+        var data = nj.zeros([typedArray.length, 2]);
+        for (i = 0; i < typedArray.length; i++) {
+            typedArray[i] = Math.abs(typedArray[i]);
+            data[i, 0] = typedArray[i];
+            data[i, 1] = typedArray[i];
         }
-        document.getElementById("values").innerText = values;
+        var foo = nj.random([typedArray.length, 2]);
+        for (i = 0; i < typedArray.length; i++) {
+            foo.set(i, 0, typedArray[i]);
+            foo.set(i, 1, typedArray[i]);
+        }
+        console.log(data);
+        console.log(foo);
+        //data = nj.concatenate(data, data);
+        //data = foo;
+        fft = nj.abs(nj.fft(foo));
+        fft.set(0, 0, 0);
+        fft.set(0, 1, 0);
+        //console.log(fft);
+        len = fft.shape[0];
+        //console.log(fft.shape[0]);
+        fft = fft.reshape(fft.shape[0] * fft.shape[1], 1).flatten().tolist();
+        //console.log(fft);
+        //document.getElementById("values").innerText = typedArray.slice(1, 10);
+        var x = nj.arange(len).tolist();
+        //console.log(x);
+        input = document.getElementById('input');
+        Plotly.plot(input,
+                    [{ x: x,
+                       y: typedArray}],
+                    { margin: { t: 0 } });
+        output = document.getElementById('output');
+        Plotly.plot(output,
+                    [{ x: x,
+                       y: fft}],
+                    { margin: { t: 0 } });
     }
 };
